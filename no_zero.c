da2c773a3047244c9a32c29b28e1173ad6b5b086
Muhammad Wali Raza <waleeraxa@gmail.com>
2025-12-26 20:56:31 +0500

    Add no_zero.c to prompt for non-zero integers

---
+++ no_zero.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+        int number=1;
+
+        while(number!=0){
+                printf("Enter an Integer: ");
+                scanf("%d", &number);
+        }
+
+        return 0;
+
+}
